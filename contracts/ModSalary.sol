d5bd8a6dd9eaa1b3b4b2fb75e40a5d1f924642bb
RSnow1031 <kevinsoft1010@gmail.com>
2021-03-02 23:30:22 +0200

    Add mod salary

---
+++ contracts/ModSalary.sol
@@ -0,0 +1,69 @@
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import './libs/IBEP20.sol';
+import './libs/SafeBEP20.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ModSalary is Ownable {
+    using SafeMath for uint256;
+    using SafeBEP20 for IBEP20;
+
+    // Info of each mod.
+    struct UserInfo {
+        uint256 lastBlockClaim; // last claimed block
+        uint256 claimPerBlock; //  block payment
+    }
+
+    // SALT TOKEN
+    IBEP20 public paymentToken;
+
+    // Info of each mod
+    mapping (address => UserInfo) public userInfo;
+
+    event Claim(address indexed user, uint256 amount);
+    event AddMod(address indexed user, uint256 claimPerBlock);
+
+    constructor(
+        IBEP20 _paymentToken,
+    ) public {
+        paymentToken = _paymentToken;
+    }
+
+    // View function to see pending Reward on frontend.
+    function pendingReward(address _user) external view returns (uint256) {
+        UserInfo storage user = userInfo[_user];
+        uint256 claimableBlocks = block.number - user.lastBlockClaim;
+        uint256 claimablePayment = claimableBlocks.mul(user.claimPerBlock)
+        
+        uint256 supply = paymentToken.balanceOf(address(this));
+        if (claimableBlocks > 0 && supply > 0) {
+            return claimablePayment;
+        } 
+        return 0
+    }
+
+    function claim() public {
+        uint256 reward = pendingReward(msg.sender);
+        if (reward > 0) {
+            UserInfo storage user = userInfo[msg.sender];
+            user.lastBlockClaim = block.number;
+            paymentToken.safeTransferFrom(address(msg.sender), address(this), reward);
+            emit Claim(msg.sender, reward);     
+        }
+    }
+
+    function addMod(address _mod, _claimPerBlock) public onlyOwner {
+        UserInfo storage user = userInfo[_mod];
+        user.lastBlockClaim = block.number;
+        user.claimPerBlock = _claimPerBlock
+        emit Deposit(_mod, _claimPerBlock);
+    }
+
+    // Withdraw reward. EMERGENCY ONLY.
+    function emergencyPaymentWithdraw(uint256 _amount) public onlyOwner {
+        require(_amount <= paymentToken.balanceOf(address(this)), 'not enough token');
+        paymentToken.safeTransfer(address(msg.sender), _amount);
+    }
+
+}
